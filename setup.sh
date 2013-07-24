cd8588579fe5340c0479878ac1dd500631a7146e
dave <dave@pixelmetrics.com>
2013-07-24 14:46:21 +0000

    added username & email

--- setup.sh
+++ setup.sh
@@ -38,10 +38,13 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/capturewiz/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+#set user name & email
+git config --global user.name dave
+git config --global user.email dave@pixelmetrics.com
